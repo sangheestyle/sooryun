d65b2c7135d8df78fa3593a50743f2e5efb5bd8e
Sanghee Kim <sangheestyle@gmail.com>
2015-06-11 16:23:54 -0600

    Strings

---
+++ java/java_vs_python/Main2.java
@@ -0,0 +1,10 @@
+public class Main2 {
+  public static void main(String[] args) {
+    // 1. Strings
+    String city = "Boulder";
+    String postalCode = "80302";
+    String address = city + " " + postalCode;
+
+    System.out.println(address);
+  }
+}
