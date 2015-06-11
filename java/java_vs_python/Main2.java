51570c8cd4f325a8a6d8a651f5171396a8bc588a
Sanghee Kim <sangheestyle@gmail.com>
2015-06-11 16:34:40 -0600

    ArrayList and List

--- java/java_vs_python/Main2.java
+++ java/java_vs_python/Main2.java
@@ -1,3 +1,7 @@
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class Main2 {
   public static void main(String[] args) {
     // 1. Strings
@@ -6,5 +10,21 @@ public class Main2 {
     String address = city + " " + postalCode;
 
     System.out.println(address);
+
+    // 4. List
+    ArrayList<Integer> aList = new ArrayList<Integer>();
+
+    aList.add(1);
+    aList.add(2);
+    aList.add(3);
+    aList.add(4);
+    aList.add(5);
+
+    // get item by index
+    int n = aList.get(0);
+
+    // get sub list
+    List<Integer> subList = aList.subList(0, 2);
+    System.out.println(subList);
   }
 }
